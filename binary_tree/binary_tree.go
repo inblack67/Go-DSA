de08859de845fb5d6a0add6085a699ce4897b374
ryuzaki <ryuzaki>
2024-01-27 01:20:56 +0530

    binary tree cont

--- binary_tree/binary_tree.go
+++ binary_tree/binary_tree.go
@@ -2,6 +2,12 @@ package binarytree
 
 import "fmt"
 
+//     5
+//    / \
+//   3   7
+//  / \
+// 2   4
+
 type Node struct {
 	data  int
 	left  *Node
@@ -52,4 +58,58 @@ func inOrder(node *Node) {
 		fmt.Println(node.data)
 		inOrder(node.right)
 	}
+}
+
+func calculateSize(root *Node) int {
+	if root == nil {
+		return 0
+	}
+	lhs := calculateSize(root.left)
+	rhs := calculateSize(root.right)
+	return lhs + rhs + 1
+}
+
+func calculateSum(root *Node) int {
+	if root == nil {
+		return 0
+	}
+	lhs := calculateSum(root.left)
+	rhs := calculateSum(root.right)
+	return lhs + rhs + root.data
+}
+
+func calculateMax(root *Node) int {
+	if root == nil {
+		return 0
+	}
+	max := root.data
+	lhs := calculateMax(root.left)
+	rhs := calculateMax(root.right)
+	var candidate int
+	if lhs > max {
+		candidate = lhs
+	} else {
+		candidate = rhs
+	}
+	if max > candidate {
+		return max
+	} else {
+		return candidate
+	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func calculateHeight(root *Node) int {
+	if root == nil {
+		return 0
+	}
+	lhs := calculateHeight(root.left)
+	rhs := calculateHeight(root.right)
+	return max(lhs, rhs) + 1
 }
